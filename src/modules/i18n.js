8c89477ca5acc3585ddddfee3afacd798a382f76
xiaominzhu <xiaominzhu@Freeman.local>
2020-10-12 10:38:19 +0200

    refactory

--- src/modules/i18n.js
+++ src/modules/i18n.js
@@ -14,9 +14,6 @@ const getTemplate = (key, rep, namespace) => {
   const BASE = MESSAGES?.[namespace] || MESSAGES;
   const template = BASE[key];
 
-  // console.log("BASE: ", BASE);
-  // console.log("template: ", template);
-
   // console.log("1-template: ", key, rep, namespace);
 
   if (!template) {
@@ -29,14 +26,14 @@ const getTemplate = (key, rep, namespace) => {
 
   const x = Object.keys(rep).map((k) => {
     if (typeof rep[k] === "number") {
-      const val = rep[k]; // [ 0,1,2,5]
+      const val = rep[k];
       const filtertemplate = BASE?.[`${key}${filters[val]}`];
 
       const pluraltemplate = (val > 1 || val === 0) && BASE?.[`${key}_plural`];
 
       return filtertemplate || pluraltemplate || template;
     }
-    return template; // default
+    return template;
   });
 
   return x[0];
@@ -54,9 +51,9 @@ const useTranslation = (namespace = null) => {
     const template = getTemplate(key, rpl, namespace);
 
     //console.log("key--rpl--namespace", key, rpl, namespace);
-    //console.log("Namespace: ", namespace);
+
     if (rpl && rpl.a === "cats" && namespace === "multimix" && key === "mm") {
-      const xx = template.replace(/{{([^{}]*)}}/g, (a, b) => {
+      const result = template.replace(/{{([^{}]*)}}/g, (a, b) => {
         const replacement = rpl[b];
         return typeof replacement === "string" ||
           typeof replacement === "number"
@@ -65,16 +62,12 @@ const useTranslation = (namespace = null) => {
       });
 
       if (rpl.b !== 1 && typeof rpl.b === "number") {
-        //console.log("replace-plural: ", xx.replace(/is/, "are"));
-        return xx.replace(/is/, "are");
+        return result.replace(/is/, "are");
       }
-      return xx;
+      return result;
     } else if (rpl) {
       return template.replace(/{{([^{}]*)}}/g, (a, b) => {
-        //console.log("a: ", a); //{{ PLMIX-DOING }}
-        //console.log("b: ", b); // PLMIX-DOING
         const replacement = rpl[b];
-
         return typeof replacement === "string" ||
           typeof replacement === "number"
           ? replacement
